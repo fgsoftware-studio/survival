2ad125673422f77cdff75ad75096feca781b3237
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2022-09-02 14:33:25 +0100

    commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+    private Vector2 m_Move;
+
+    public void Update()
+    {
+        Move(m_Move);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        m_Move = context.ReadValue<Vector2>();
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if (direction.sqrMagnitude < 0.01)
+            return;
+        var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+
+        var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
+        transform.position += move * scaledMoveSpeed;
+    }
+}
