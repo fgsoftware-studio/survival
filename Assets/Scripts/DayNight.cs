a59a5337bbc3e59c8939eec04bf401d4aa3355b9
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2022-09-11 20:10:10 +0100

    commit

---
+++ Assets/Scripts/DayNight.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+    private const int minInDay = 1440;
+    public Transform sun;
+    public float x_speed = 5f;
+    public float y_speed = 5f;
+
+    [Range(0, 5)] public float minToSecond = 1f;
+
+    [SerializeField] private int currentMin;
+
+    private int dayCount;
+    private float timeSinceLastMin;
+
+
+    private void Update()
+    {
+        var t = Time.deltaTime;
+
+        sun.Rotate(new Vector3(x_speed * t, y_speed * t, 0f));
+
+        if (timeSinceLastMin >= minToSecond)
+        {
+            timeSinceLastMin = 0f;
+            MinutePassed();
+        }
+        else
+        {
+            timeSinceLastMin += t;
+        }
+    }
+
+    private void MinutePassed()
+    {
+        ++currentMin;
+        if (currentMin >= minInDay)
+        {
+            ++dayCount;
+            currentMin = 0;
+            Debug.Log("NextDay!");
+        }
+
+        var min = currentMin % 60;
+        var hrs = currentMin / 60;
+
+        Debug.Log("Next minute! " + hrs + "   :   " + min);
+    }
+}
